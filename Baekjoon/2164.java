180c2a7093d21cd6aedc05c01f4579eff8f0c637
Sujin Gwak <kasiuky@nate.com>
2023-04-19 02:29:38 +0900

    2023.04.19 카드2

---
+++ Baekjoon/2164.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Queue<Integer> myQueue = new LinkedList<>();
+        int num = scanner.nextInt();
+
+        for(int i=1; i<=num; i++){
+            myQueue.add(i);
+        }
+
+        while(myQueue.size() > 1){
+            myQueue.poll();
+            myQueue.add(myQueue.poll());
+        }
+        System.out.println(myQueue.poll());
+    }
+}
