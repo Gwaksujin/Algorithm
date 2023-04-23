139e242a811b29018d4fb33e2523023fdf055c2d
Sujin Gwak <kasiuky@nate.com>
2023-04-23 12:32:46 +0900

    2023.04.23 소트인사이드

---
+++ Baekjoon/1427.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int[] arr = new int[str.length()];
+
+        for(int i=0; i<str.length(); i++){
+            arr[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        for(int i=0; i<str.length(); i++){
+            int max = i;
+            for(int j=i+1; j<str.length(); j++){
+                if(arr[j] > arr[max])
+                    max = j;
+            }
+            if(arr[i]<arr[max]){
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+            }
+        }
+        for(int i=0; i<str.length(); i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
